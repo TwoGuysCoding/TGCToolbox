fdcbb2e5b1838cdc23c068157b3a38f8c3c8728e
mvishiu11 <116217532+mvishiu11@users.noreply.github.com>
2024-05-05 13:34:07 +0200

    feat: add predefined file load based on APP_ENVIRONMENT, add docs

--- tgctoolbox/settings.py
+++ tgctoolbox/settings.py
@@ -1,5 +1,7 @@
 import logging
 import os
+from typing import List
+from typing import Optional
 
 import yaml
 
@@ -26,18 +28,34 @@ class SettingsMeta(type):
 class Settings(metaclass=SettingsMeta):
     """Class to manage application settings."""
 
-    def __init__(self, config_files=None, reload=False, load_env=False):
+    def __init__(
+        self,
+        load_predefined: bool = True,
+        reload: bool = False,
+        load_env: bool = False,
+        base_dir: Optional[str] = None,
+        config_files: Optional[List[str]] = None,
+    ):
         """
-        Initialize Settings object.
+        Initialize Settings object. Load settings from YAML files and environment variables.
+
+        Note: The settings are loaded from the following sources:
+        1. Predefined YAML files (base.yaml, local.yaml, production.yaml) - this setting is enabled by default and uses the APP_ENVIRONMENT environment variable to choose the YAML files based on convention
+        2. Custom YAML files - pass the list of paths to the YAML files to load and set load_predefined to False
+        3. Environment variables - set load_env to True to load settings from environment variables, which should be prefixed with 'VAR_' as per convention
 
         Args:
-            config_files (list): List of YAML configuration files.
-            reload (bool): Whether to reload settings.
+            load_predefined (bool): Load settings from predefined YAML files. Default is True.
+            reload (bool): Whether to reload settings. Default is False.
+            load_env (bool): Load settings from environment variables. Default is False.
+            base_dir (str): Path to the directory containing the YAML files. Default is 'configuration'.
+            config_files (list): List of paths to the YAML files to load. By default, it loads 'base.yaml', 'local.yaml', and 'production.yaml'.
         """
+        BASE_DIR = base_dir or "configuration"
         BASE_FILES = [
-            os.path.join("configuration", "base.yaml"),
-            os.path.join("configuration", "local.yaml"),
-            os.path.join("configuration", "production.yaml"),
+            os.path.join(BASE_DIR, "base.yaml"),
+            os.path.join(BASE_DIR, "local.yaml"),
+            os.path.join(BASE_DIR, "production.yaml"),
         ]
 
         REQUIRED_SETTINGS = ["port", "host"]
@@ -47,7 +65,10 @@ class Settings(metaclass=SettingsMeta):
             self.host = None
             self._settings = {}
             config_files = config_files or BASE_FILES
-            self._load_from_yaml(config_files)
+            if load_predefined:
+                self._load_from_yaml_predefined(BASE_DIR)
+            else:
+                self._load_from_yaml(config_files)
             if load_env:
                 self._load_from_env()
             self._initialized = True
@@ -60,6 +81,26 @@ class Settings(metaclass=SettingsMeta):
                 raise ValueError(f"Setting '{setting}' is required")
             else:
                 setattr(self, setting, self.get(setting))
+
+    def _load_from_yaml_predefined(self, dir_path):
+        """Load settings from predefined YAML files.
+
+        Note: This method chooses the YAML files based on the APP_ENVIRONMENT environment variable.
+        APP_ENVIRONMENT can be 'local' or 'production'. If not set, 'local' is used by default.
+
+        Args:
+            dir_path (str): Path to the directory containing the YAML files.
+        """
+        base_path = os.path.join(dir_path, "base.yaml")
+        local_path = os.path.join(dir_path, "local.yaml")
+        prod_path = os.path.join(dir_path, "production.yaml")
+        app_env = os.environ.get("APP_ENVIRONMENT", "local")
+        if app_env not in ["local", "production"]:
+            raise ValueError(
+                "Invalid APP_ENVIRONMENT value. Use 'local' or 'production'."
+            )
+        FILES = [base_path, local_path if app_env == "local" else prod_path]
+        self._load_from_yaml(FILES)
 
     def _load_from_yaml(self, config_files):
         """Load settings from YAML files."""
@@ -125,7 +166,7 @@ if __name__ == "__main__":
     settings2 = Settings()
 
     # Accessing settings
-    app_port = settings.get("port")
-    app_host = settings.get("host")
+    app_port = settings.port
+    app_host = settings.host
     print(f"App Port: {app_port}, App Host: {app_host}")
     print(settings is settings2)
